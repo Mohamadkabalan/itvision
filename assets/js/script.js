86646702b1a817b66b80fb97f9bb3af10a2d7a24
Mohamad Kabalane <mkabalane@gmail.com>
2022-02-27 14:23:50 +0200

    add fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
         $.ajax({
             url: "jobs.php",
             method: "POST",
+            contentType: false,
+            cache: false,
+            processData:false,
             data:$('#careers_form').serialize(),
             success: function (data) {
                 console.log(data);
